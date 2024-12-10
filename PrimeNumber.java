d1ac08cc7cbe87bcdf598e44f76e7b95feb3d4bb
kiranmadine <kiranmadine878@gmail.com>
2024-12-10 10:57:54 +0530

    no

---
+++ PrimeNumber.java
@@ -0,0 +1,17 @@
+public class PrimeNumber{
+    public static void main(String[] args) {
+        System.out.println("prime numbers between 1 and 100");
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
